63cc7a4ca8cf7e0d8b298892bae1d18beed9a4ee
Pra04Gya <157507407+Pra04Gya@users.noreply.github.com>
2025-05-12 13:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000],
-	  1:['CM',900],
-      2:['D', 500],
-	  3:['CD',400],
-      4:['C', 100],
-	  5:['XC',90],
-      6:['L', 50], 
-      7:['XL',49], 
-      8:['X',10], 
-      9:['IX',9],
-	  10:['V',5],
-	  11:['IV',4],
-	  12:['I',1]
-	};
+  const obj = {
+    0: ['M', 1000],
+    1: ['CM', 900],
+    2: ['D', 500],
+    3: ['CD', 400],
+    4: ['C', 100],
+    5: ['XC', 90],
+    6: ['L', 50],
+    7: ['XL', 40],
+    8: ['X', 10],
+    9: ['IX', 9],
+    10: ['V', 5],
+    11: ['IV', 4],
+    12: ['I', 1]
+  };
 
-  let n = 986;
-	let ans=""
-	  while(n!=0){
-		  for(let i in obj){
-                let value=obj[i][0];
-			    let num=obj[i][0];
-			    if(num<=n)
-				{
-                      ans=ans+value;
-					  n=n-num;
-					break;
-				}
-		  }
-	  }
-	  
+  let ans = "";
+  for (let i in obj) {
+    let symbol = obj[i][0];
+    let value = obj[i][1];
 
+    while (num >= value) {
+      ans += symbol;
+      num -= value;
+    }
+  }
+
+  return ans;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Example:
+console.log(convertToRoman(986));  // Output: "CMLXXXVI"
